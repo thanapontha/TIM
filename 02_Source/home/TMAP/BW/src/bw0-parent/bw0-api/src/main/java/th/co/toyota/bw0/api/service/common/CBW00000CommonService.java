53f5fec78807abcff85f83dded9efa31393edc09
Thanawut T <thanawut.thong@gmail.com>
2019-01-10 17:03:31 +0700

    update

--- 02_Source/home/TMAP/BW/src/bw0-parent/bw0-api/src/main/java/th/co/toyota/bw0/api/service/common/CBW00000CommonService.java
+++ 02_Source/home/TMAP/BW/src/bw0-parent/bw0-api/src/main/java/th/co/toyota/bw0/api/service/common/CBW00000CommonService.java
@@ -25,8 +25,6 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
